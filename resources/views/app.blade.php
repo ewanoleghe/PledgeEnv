4333a0a4dedcda07e2c1298397310f3c985f3aa1
Ewan Oleghe <ewan.mails@gmail.com>
2025-04-04 13:58:16 -0400

    Updated meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    {{-- font-awesome  --}}
+    <meta name="description" content="Pledge Environmental promotes environmental safety and public health through expert lead evaluation and assessment service to residential, governmental, and commercial clients throughout the U.S." />
+  {{-- font-awesome  --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" 
     integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" 
     crossorigin="anonymous" referrerpolicy="no-referrer" />
